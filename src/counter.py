b87070cf15afdc3bbc68ab3ee5a1a2972bbc0876
Courtney Mcgee <mcgeec4@ad.unlv.edu>
2024-09-24 20:10:01 -0700

    Workflow

--- src/counter.py
+++ src/counter.py
@@ -17,10 +17,12 @@ def create_counter(name):
         return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
-    COUNTERS[name]=COUNTERS[name] +1
+    COUNTERS[name] = COUNTERS[name] + 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
+
 @app.route('/counters/<name>', methods=['GET'])
 def get_counter(name):
     if name in COUNTERS:
